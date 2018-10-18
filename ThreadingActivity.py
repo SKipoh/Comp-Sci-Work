a154af32afd90b6a6d78d8d32ed6515e6bd24011
Jack Burne <jack_burne@yahoo.co.uk>
2018-10-18 21:14:16 +0100

    Added comments to the code, also removed "count" variable, in favour of just using the "i" variable that get's incremented anyway

--- ThreadingActivity.py
+++ ThreadingActivity.py
@@ -1,16 +1,26 @@
 import time
 from threading import Thread
 
-
-
-def myfunc(i, count):
+# Declaring a function called "myfunc", that needs a
+# variable "i" parsed to it
+def myfunc(i):
+    # Prints out a statement, using the "i" varible
+    # to show the thread number
     print("sleeping 5 sec from thread %d" % i)
-    time.sleep(count)
+    # Sleeps for the amount of time that "i"
+    time.sleep(i)
+    # Prints a statement telling the user thread for
+    # the value of "i" has been closed
     print("finished sleeping from thread %d" % i)
 
-count = 1
-
+# For loop that iterates 10 times, starting a new thread
+# for each time it runs "myfunc", as well as iterating
+# the "count" variable
 for i in range(10):
-    t = Thread(target=myfunc, args=(i, count))
+    # Creates a new Thread Object, assigned to "t",
+    # the thread will run the "myfunc" function, parsing
+    # the "i" varible as an argument
+    t = Thread(target=myfunc, args=(i,))
+    # Starts the Thread using the the target and
+    # arguments in the "Thread" object
     t.start()
-    count += 1
