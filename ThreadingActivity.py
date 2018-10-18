8137439df6eb4477475d501f36e6cf9c1e953b3b
Jack Burne <jack_burne@yahoo.co.uk>
2018-10-18 20:35:57 +0100

    Changed Code To Make Each Thread End After The Number of Seconds It Displays

--- ThreadingActivity.py
+++ ThreadingActivity.py
@@ -1,11 +1,16 @@
 import time
 from threading import Thread
 
-def myfunc(i):
-    print("/nsleeping 5 sec from thread %d" % i)
-    time.sleep(5)
-    print("/nfinished sleeping from thread %d" % i)
+
+
+def myfunc(i, count):
+    print("sleeping 5 sec from thread %d" % i)
+    time.sleep(count)
+    print("finished sleeping from thread %d" % i)
+
+count = 1
 
 for i in range(10):
-    t = Thread(target=myfunc, args=(i,))
+    t = Thread(target=myfunc, args=(i, count))
     t.start()
+    count += 1
